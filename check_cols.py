91a551f17cebec4b49eb62636edcc590896990e2
LakshnyaAdhikari <lakshnyaadhikari1@gmail.com>
2026-02-23 18:29:09 +0530

    chore: clean gitignore, remove debug/test/temp files from tracking

--- check_cols.py
+++
@@ -1,8 +0,0 @@
-from score_service import engine
-import pandas as pd
-
-df = pd.read_sql("SELECT * FROM fund_features LIMIT 0", engine)
-print("Columns in fund_features:", df.columns.tolist())
-
-df2 = pd.read_sql("SELECT * FROM funds LIMIT 0", engine)
-print("Columns in funds:", df2.columns.tolist())
